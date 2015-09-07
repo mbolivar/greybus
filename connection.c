mbolivar/greybus

commit 5313ca607afba22ae66e39166fce898e6324a68e

Greybus driver: add a new callbacks to driver
Add connection_create and connection_destroy callbacks.

ES2 can map a cport to a pair of endpoints.
Because ES2 have only a few pair of endpoints, ES2 need to have
access to some high level connection information such as protocol id
to effectively map the cports.
These callback will provide these information and help ES2 to map cports.

Signed-off-by: Alexandre Bailon <abailon@baylibre.com>
Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>

connection.c
@@ -262,6 +262,8 @@ gb_connection_create_range(struct greybus_host_device *hd,
 		gb_svc_connection_create(hd->svc,
 					 hd->endo->ap_intf_id, hd_cport_id,
 					 bundle->intf->interface_id, cport_id);
+		if (hd->driver->connection_create)
+			hd->driver->connection_create(connection);
 	}
 
 	gb_connection_bind_protocol(connection);
@@ -429,6 +431,9 @@ void gb_connection_destroy(struct gb_connection *connection)
 	list_del(&connection->bundle_links);
 	list_del(&connection->hd_links);
 	spin_unlock_irq(&gb_connections_lock);
+
+	if (connection->hd->driver->connection_destroy)
+		connection->hd->driver->connection_destroy(connection);
 
 	if (connection->protocol)
 		gb_protocol_put(connection->protocol);

greybus.h
@@ -75,6 +75,8 @@ struct greybus_host_device;
 struct greybus_host_driver {
 	size_t	hd_priv_size;
 
+	void (*connection_create)(struct gb_connection *connection);
+	void (*connection_destroy)(struct gb_connection *connection);
 	int (*message_send)(struct greybus_host_device *hd, u16 dest_cport_id,
 			struct gb_message *message, gfp_t gfp_mask);
 	void (*message_cancel)(struct gb_message *message);

commit 555aab6ba05dc5faf9c16dbbfa20a1a1aba01f69

connection: Call connection_destroy() from gb_connection_svc_connection_destroy()
connection_create() is called right after svc is requested to create the
connection and so connection_destroy() must be called just before we
request the SVC to destroy the connection.

Over that, this fixes the inconsistency where connection_create() is
called for all connections except SVC connection, but
connection_destroy() is called always.

Cc: Alexandre Bailon <abailon@baylibre.com>
Fixes: 5313ca607afb ("Greybus driver: add a new callbacks to driver")
Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>

connection.c
@@ -318,6 +318,9 @@ gb_connection_svc_connection_destroy(struct gb_connection *connection)
 	if (connection->hd_cport_id == GB_SVC_CPORT_ID)
 		return;
 
+	if (connection->hd->driver->connection_destroy)
+		connection->hd->driver->connection_destroy(connection);
+
 	gb_svc_connection_destroy(connection->hd->svc,
 				  connection->hd->endo->ap_intf_id,
 				  connection->hd_cport_id,
@@ -461,9 +464,6 @@ void gb_connection_destroy(struct gb_connection *connection)
 	list_del(&connection->bundle_links);
 	list_del(&connection->hd_links);
 	spin_unlock_irq(&gb_connections_lock);
-
-	if (connection->hd->driver->connection_destroy)
-		connection->hd->driver->connection_destroy(connection);
 
 	if (connection->protocol)
 		gb_protocol_put(connection->protocol);